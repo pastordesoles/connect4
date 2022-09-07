b42b19d55a92f9e316e6799859520af3f3c4e867
pastordesoles <xavi.sans.b@gmail.com>
2022-09-07 18:57:08 +0200

    fixed color fill in js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const holes = document.getElementsByClassName('hole')
-let currentPlayer = 1
+const holes = document.getElementsByClassName("hole");
+let currentPlayer = 1;
 
-for (const hole of holes){
-hole.addEventListener('click', () => {
-hole.classList.add("player-one");
-    });
+for (const hole of holes) {
+  hole.addEventListener("click", () => {
+    if (currentPlayer === 1) {
+      hole.classList.add("player-one");
+      currentPlayer = 2;
+    } else if (currentPlayer === 2) {
+      hole.classList.add("player-two");
+      currentPlayer = 1;
+    }
+  });
 }
-
-  
-                
-                  
