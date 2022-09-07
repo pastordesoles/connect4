5ed742f724fefc5933ce42be3fd9a5ccfc7a5440
pastordesoles <xavi.sans.b@gmail.com>
2022-09-07 19:18:50 +0200

    prevent override

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ let currentPlayer = 1;
 
 for (const hole of holes) {
   hole.addEventListener("click", () => {
-    if (currentPlayer === 1) {
-      hole.classList.add("player-one");
+    if (currentPlayer === 1 && !hole.classList.contains("taken")) {
+      hole.classList.add("player-one")
+      hole.classList.add("taken");
       currentPlayer = 2;
-    } else if (currentPlayer === 2) {
-      hole.classList.add("player-two");
+    } else if (currentPlayer === 2 && !hole.classList.contains("taken")) {
+      hole.classList.add("player-two")
+      hole.classList.add("taken");
       currentPlayer = 1;
     }
   });
